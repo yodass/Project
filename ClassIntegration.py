11244583f6bdd180d407fc842c61e1548a09cbdd
Yodas <k.kosmik@mail.ru>
2016-02-14 18:10:13 +0300

    4 up

--- ClassIntegration.py
+++ ClassIntegration.py
@@ -1,2 +1,12 @@
+x = 10
 class Integration(object):
-    pass
+    x = 10
+    def __init__(self):
+        self.x = 20
+
+    def print(self):
+        print(self.x)
+
+print(x)
+Class = Integration()
+Class.print()
